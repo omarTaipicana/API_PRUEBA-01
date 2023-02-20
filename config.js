45b2117e517db0601f6466ea5d953fc209265930
ferTaipi <fernandoparadoja@gmail.com>
2023-02-20 09:55:07 -0500

    20/02/2023

--- config.js
+++ config.js
@@ -6,21 +6,39 @@ const configs = {
         port: process.env.PORT,
         host: process.env.HOST
     },
-    db:{
-        developent: {
+    db: {
+        development: {
             dialect: "postgres",
             host: "localhost",
             username: "postgres",
             password: "root",
-            port:5432,
+            port: 5432,
             database: "coordenadas-db",
             define: {
-                timestamps: true,  
+                timestamps: true,
                 underscored: true,
                 underscoredAll: true,
-
             }
-
+        },
+        production: {
+            dialect: "postgres",
+            hostname: "dpg-cfn49mqrrk0eqlu79jt0-a",
+            port: 5432,
+            username: "prueba_cfek_user",
+            password: "QZwZdKqRJ33997KbwG9eDiAisn5BqF2X",
+            database: "prueba_cfek",
+            define: {
+                timestamps: true,
+                underscored: true,
+                underscoredAll: true
+            }
+            // ,
+            // dialectOptions: {
+            //     ssl: {
+            //         require: true,
+            //         rejectUnauthorized: false
+            //     }
+            // }
         }
 
     }
--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@ const {Sequelize} = require("sequelize")
 
 const config = require("../../config")
 
-const db = new Sequelize(config.db.developent)
+const db = new Sequelize(config.db[config.api.nodeEnv])
 
 module.exports = db
 
